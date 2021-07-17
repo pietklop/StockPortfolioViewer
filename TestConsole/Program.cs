edeaaaab7aed111fecb954ed7dedabf7ee18b095
Piet Klop <p.klop@viscon.eu>
2021-07-17 08:48:54 +0200

    Testconsole cleanup

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using Castle.MicroKernel;
 using DAL;
+using DAL.Entities;
 using Imports.DeGiro;
 using log4net;
 using log4net.Config;
+using Messages.StockDataApi;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
 using Services;
+using Services.DI;
+using Services.Helpers;
+using StockDataApi.AlphaVantage;
+using StockDataApi.General;
+using StockDataApi.IexCloud;
+using StockDataApi.MarketStack;
+using StockDataApi.TwelveData;
 
 namespace TestConsole
 {
@@ -20,44 +34,134 @@ namespace TestConsole
             XmlConfigurator.Configure(new FileInfo(@"log4net.config"));
             log.Info("Start");
 
-            var importer = new Importer();
-            var import = importer.Import(@"c:\Users\pieterk\Dropbox\Aandelen\Account (2).csv");
-
-            //return;
-
-            using (var db = new StockDbContext())
-            {
-                db.Database.Migrate();
-
-                var stockService = new StockService(log, db);
-                var importProcessor = new ImportProcessor(log, db, stockService);
-                importProcessor.Process(import);
-
-                //Setup(db);
-
-                db.SaveChanges();
-            }
+//            RemoveLargePositions();
+//            Test();
+//            Setup();
+//            Migrate();
 
             Console.WriteLine("Done");
             Console.ReadKey();
         }
 
-        public static void Setup(StockDbContext db)
+        public static void RemoveLargePositions(float removeLargerThan)
         {
-            AreaSetup.AddContinents(db);
-            CurrencySetup.AddCurrencies(db);
-            SectorSetup.AddSectors(db);
+            log.Info($"Remove positions larger than: {removeLargerThan}");
+            using (var db = new StockDbContext())
+            {
+                var stocks = db.Stocks
+                    .Include(s => s.AreaShares)
+                    .Include(s => s.LastKnownStockValue.StockValue)
+                    .Include(s => s.SectorShares)
+                    .Include(s => s.Transactions)
+                    //.Where(s => s.Name == "MSFT")
+                    .ToList();
+
+                var largeStocks = new List<Stock>();
+                foreach (var stock in stocks)
+                {
+                    if (Value(stock) > removeLargerThan)
+                        largeStocks.Add(stock);
+                }
+
+                foreach (var stock in largeStocks)
+                    stock.LastKnownStockValue = null;
+
+                db.SaveChanges();
+
+                foreach (var stock in largeStocks)
+                    RemoveStock(stock, db);
+
+                db.SaveChanges();
+            }
+
+            double Value(Stock stock)
+            {
+                if (stock.LastKnownStockValue == null) return 100_000;
+                return stock.Transactions.Sum(t => t.Quantity) * stock.LastKnownUserPrice;
+            }
+
+            void RemoveStock(Stock stock, StockDbContext db)
+            {
+                log.Info($"Remove: {stock}");
+                var divs = db.Dividends.Where(d => d.StockId == stock.Id).ToList();
+                db.RemoveRange(divs);
+
+                var trans = db.Transactions.Where(d => d.StockId == stock.Id).ToList();
+                db.RemoveRange(trans);
+
+                var pits = db.PitStockValues.Where(d => d.StockId == stock.Id).ToList();
+                db.RemoveRange(pits);
+
+                var comps = db.DataRetrievers.SelectMany(d => d.StockRetrieverCompatibilities).Where(d => d.StockId == stock.Id).ToList();
+                db.RemoveRange(comps);
+
+                db.RemoveRange(stock.AreaShares);
+                db.RemoveRange(stock.SectorShares);
+
+                db.Stocks.Remove(stock);
+            }
         }
 
+        public static void Test()
+        {
+            var container = CastleContainer.Instance;
+            var installer = DependencyInstaller.CreateInstaller();
+            container.AddFacilities().Install(installer);
+            using (var db = new StockDbContext())
+            {
+                //            var dr = CastleContainer.Resolve<IexDataRetriever>();
 
-        //        private static IWindsorContainer BuildContainer()
-        //        {
-        //            var container = new WindsorContainer();
-        //            container.Kernel.Resolver.AddSubResolver(new CollectionResolver(container.Kernel));
-        //
-        //            return container.Install(new DependencyInstaller(new DbContextOptionsBuilder()
-        //                    .UseSqlite("Filename=StockDatabase.db").Options));
-        //        }
+//                var pp = db.PitStockValues.ToList();
+//                var pitGroups = pp.GroupBy(p => p.StockId).ToList();
+//                foreach (var pits in pitGroups)
+//                {
+//                    var dd = DateTime.Now.AddDays(2).Date;
+//                    foreach (var p in pits.OrderByDescending(p => p.TimeStamp))
+//                    {
+//                        if (p.TimeStamp.Date == dd)
+//                            db.Remove(p);
+//                            //log.Info($"Remove Pit: {p.Id}");
+//                        else
+//                            dd = p.TimeStamp.Date;
+//                    }
+//                }
+
+                db.SaveChanges();
+
+            }
+        }
+
+        public static void Migrate()
+        {
+            using (var db = new StockDbContext())
+            {
+                log.Info($"Migrate...");
+                db.Database.Migrate();
+
+
+                db.SaveChanges();
+            }
+        }
+
+        public static void Setup()
+        {
+            using (var db = new StockDbContext())
+            {
+                log.Info($"Migrate and setup");
+                db.Database.Migrate();
+
+                DataRetrieverSetup.AddRetrievers(db);
+                AreaSetup.AddContinents(db);
+                CurrencySetup.AddCurrencies(db);
+                SectorSetup.AddSectors(db);
+
+                db.SaveChanges();
+            }
+
+        }
 
     }
 }
+
+
+
