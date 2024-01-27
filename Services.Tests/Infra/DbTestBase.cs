1ee6df4a94c707e8bd8dc2d9f850a3e1b9bcb411
Piet Klop <p.klop@viscon.eu>
2024-01-27 09:11:02 +0100

    Add project Services.Tests

---
+++ Services.Tests/Infra/DbTestBase.cs
@@ -0,0 +1,34 @@
+﻿using DAL;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Services.Tests.Infra
+{
+    [SuppressMessage("ReSharper", "ConsiderUsingConfigureAwait")]
+    public abstract class DbTestBase
+    {
+        public DbContextOptions<StockDbContext> Options = new DbContextOptionsBuilder<StockDbContext>()
+            .EnableSensitiveDataLogging()
+            .UseSqlite(CreateInMemoryDatabase())
+            .Options;
+
+        private static DbConnection CreateInMemoryDatabase()
+        {
+            var connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+
+            return connection;
+        }
+
+        protected StockDbContext CreateDbContext()
+        {
+            var dbContext = new StockDbContext(Options);
+            dbContext.Database.OpenConnection();
+            dbContext.Database.EnsureCreated();
+
+            return dbContext;
+        }
+    }
+}
