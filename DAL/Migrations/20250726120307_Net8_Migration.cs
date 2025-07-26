b72fe8308548b3ee0bf36888be6ac0199f386292
Piet Klop <p.klop@viscon.eu>
2025-07-26 14:13:02 +0200

    Migration: due to earlier EF core update

---
+++ DAL/Migrations/20250726120307_Net8_Migration.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Net8_Migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Stocks_Currencies_CurrencyKey",
+                table: "Stocks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CurrencyKey",
+                table: "Stocks",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Stocks_Currencies_CurrencyKey",
+                table: "Stocks",
+                column: "CurrencyKey",
+                principalTable: "Currencies",
+                principalColumn: "Key",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Stocks_Currencies_CurrencyKey",
+                table: "Stocks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CurrencyKey",
+                table: "Stocks",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Stocks_Currencies_CurrencyKey",
+                table: "Stocks",
+                column: "CurrencyKey",
+                principalTable: "Currencies",
+                principalColumn: "Key");
+        }
+    }
+}
